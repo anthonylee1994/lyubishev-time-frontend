5f55e66fa886948e2c0fdb862005e42ceaf57856
Anthony Lee <hososlch@gmail.com>
2024-01-03 22:18:58 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig} from "vite";
+import {VitePWA} from "vite-plugin-pwa";
+import React from "@vitejs/plugin-react";
+import CommonJS from "vite-plugin-commonjs";
+import Legacy from "@vitejs/plugin-legacy";
+import TSConfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        React(),
+        CommonJS(),
+        VitePWA({
+            registerType: "autoUpdate",
+        }),
+        TSConfigPaths(),
+        Legacy({targets: ["cover 95%"]}),
+    ],
+});
