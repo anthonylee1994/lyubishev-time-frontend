2269a71bb085281f3d51274c6ebbfd21584084be
Anthony Lee <hososlch@gmail.com>
2024-01-04 19:04:36 +0800

    feat: event tag

--- src/pages/login/LoginButton.tsx
+++ src/pages/login/LoginButton.tsx
@@ -4,17 +4,17 @@ import {Box, CircularProgress} from "@mui/material";
 import {useAuthStore} from "../../store/useAuthStore.ts";
 import {useNavigate} from "react-router-dom";
 
-const mockUser = {
-    id: 234392020,
-    first_name: "窮三代IT狗",
-    username: "internal_server_error",
-    photo_url: "https://t.me/i/userpic/320/0HsMKYOWlgnBA2Httlfb3NBMp50KmdI4fD8tsNZu2js.jpg",
-    auth_date: 1704334989,
-    hash: "1a5186eae36d58a37868d019b8037a4dc02980e6f3e62cb124a80e8bec6b732d",
-};
+// const mockUser = {
+//     id: 234392020,
+//     first_name: "窮三代IT狗",
+//     username: "internal_server_error",
+//     photo_url: "https://t.me/i/userpic/320/0HsMKYOWlgnBA2Httlfb3NBMp50KmdI4fD8tsNZu2js.jpg",
+//     auth_date: 1704334989,
+//     hash: "1a5186eae36d58a37868d019b8037a4dc02980e6f3e62cb124a80e8bec6b732d",
+// };
 
 export const LoginButton = React.memo(() => {
-    const [showLoading, setShowLoading] = React.useState(true);
+    const [showLoading, setShowLoading] = React.useState(false);
     const isLoggedIn = useAuthStore(state => state.isLoggedIn);
     const login = useAuthStore(state => state.login);
 
@@ -28,11 +28,11 @@ export const LoginButton = React.memo(() => {
         }
     };
 
-    React.useEffect(() => {
-        setTimeout(() => {
-            onLogin(mockUser);
-        }, 3000);
-    }, []);
+    // React.useEffect(() => {
+    //     setTimeout(() => {
+    //         onLogin(mockUser);
+    //     }, 3000);
+    // }, []);
 
     return (
         <Box position="fixed" bottom={{xs: "10%", md: "15%"}}>
