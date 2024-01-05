d2a1be66ceed3c7c3582f958206c9d12ad9e7c77
Anthony Lee <hososlch@gmail.com>
2024-01-05 11:16:01 +0800

    feat: tune Page.tsx

--- src/component/Page.tsx
+++ src/component/Page.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const Page = React.memo<Props>(({children}) => {
     return (
-        <Box position="relative" mt={{xs: 7, sm: 8}} mb={{ xs: 30, sm: 30 }} maxWidth={600} mx="auto">
+        <Box position="relative" mt={{xs: 7, sm: 8}} pb="max(22vh, 180px)" maxWidth={600} mx="auto">
             {children}
         </Box>
     );
