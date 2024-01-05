7fe33893aa62838c1081f36721823e291b3ecf67
Anthony Lee <hososlch@gmail.com>
2024-01-05 18:18:14 +0800

    fix: minor

--- src/component/Layout/TitleBar/index.tsx
+++ src/component/Layout/TitleBar/index.tsx
@@ -7,26 +7,37 @@ export const TitleBar = React.memo(() => {
     return (
         <AppBar position="fixed" sx={{bgcolor: blueGrey[50]}}>
             <Toolbar sx={{bgcolor: blueGrey[50]}}>
-                <Box maxWidth={580} mx="auto" width="100%" display="flex" justifyContent="space-between" alignItems="center">
+                <Box
+                    maxWidth={580}
+                    mx="auto"
+                    width="100%"
+                    display="flex"
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
                     <Box display="flex" alignItems="flex-end">
                         <Typography
                             variant="h6"
                             component="div"
                             sx={{
+                                userSelect: "none",
                                 fontSize: {xs: 20, sm: 26},
                                 color: grey[900],
                                 fontWeight: "bold",
                                 mr: 1,
-                            }}>
+                            }}
+                        >
                             柳比歇夫
                         </Typography>
                         <Typography
                             component="div"
                             sx={{
+                                userSelect: "none",
                                 fontSize: {xs: 16, sm: 18},
                                 color: grey[700],
                                 mb: {base: 0.1, sm: 0.2},
-                            }}>
+                            }}
+                        >
                             時間管理大師
                         </Typography>
                     </Box>
